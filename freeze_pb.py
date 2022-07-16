5e2087a70ff2cf981454ad41a8439ff82d97b9da
anbaolei <litherain@zhaoxiaoqiandeMacBook-Air.local>
2022-07-16 14:38:39 +0800

    ii

--- freeze_pb.py
+++ freeze_pb.py
@@ -9,6 +9,7 @@ def freeze_graph(input_checkpoint, meta_path, output_node_names, output_graph):
     saver = tf.train.import_meta_graph(meta_path, clear_devices = True)
     with tf.Session() as sess:
         saver.restore(sess, input_checkpoint)
+        print (sess.graph_def)
         output_graph_def = graph_util.convert_variables_to_constants(  
             sess = sess,
             input_graph_def = sess.graph_def,
@@ -26,12 +27,5 @@ input_checkpoint = sys.argv[1]
 meta_path = sys.argv[2]
 output_node_names = sys.argv[3]
 output_graph = sys.argv[4]
-
-# print all nodes in network
-reader = tf.train.NewCheckpointReader(input_checkpoint)
-var_to_shape_map = reader.get_variable_to_shape_map()
-for key in var_to_shape_map:
-    print ('tensor name :' key)
-
 
 freeze_graph(input_checkpoint, meta_path, output_node_names, output_graph)
